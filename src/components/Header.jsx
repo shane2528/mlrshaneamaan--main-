f3e971ae78f99ac0b995acdc6fec2ffefb395119
RafathAmaan Mohammad <116374643+rafathamaan@users.noreply.github.com>
2023-03-04 00:59:20 +0530

    s

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -155,7 +155,7 @@ const Header = () => {
         <div className="relative">
           <motion.img
             whileTap={{ scale: 0.6 }}
-            src={user ? user.photoURL : ""}
+            src={user ? user.photoURL : Avatar}
             className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
             alt="userprofile"
             onClick={login}
