fc97b45cbb5454ef457b6de65b805db83266d1ff
shane2528 <ussr2528@gmail.com>
2023-03-03 15:13:09 +0530

    profile fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -98,7 +98,7 @@ const Header = () => {
           <div className="relative">
             <motion.img
               whileTap={{ scale: 0.6 }}
-              src={user ? user.photoURL : Avatar}
+              src={user ? user.photoURL : ""}
               className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
               alt="userprofile"
               onClick={login}
